2372403b5a10fa7630c55859e6010ddf2d323ef1
hugoeckenschwiller <hu.eckenschwiller@gmail.com>
2024-12-08 22:25:50 +0100

    fix

--- db/__init__.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config.env_config import PYTHON_DATASOURCE_URL
-from models.base import Base
-engine = create_engine(PYTHON_DATASOURCE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
