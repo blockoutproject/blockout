f19e1c50ae0df6c4b28959e9f1beecc39ee67b85
hugoeckenschwiller <hu.eckenschwiller@gmail.com>
2024-12-20 12:08:31 +0100

    add secu

--- api/poolsApi.ts
+++ api/poolsApi.ts
@@ -1,22 +1,53 @@
 import { CONFIG } from '@/config/config';
-import { createApiClient } from '../config/apiClient';
-import { Pool } from '../types/Pool';
+import AbstractApi from './AbstractApi';
+import { Pool } from '@/types/Pool';
 
-const api = createApiClient(CONFIG.API_POOLS_BASE_URL);
+class PoolsApi extends AbstractApi {
+    private static instance: PoolsApi | null = null;
 
-export const poolsApi = {
-    getAllPools: async (): Promise<Pool[]> => {
-        const response = await api.get<Pool[]>('/');
+    private constructor(url: string, token: string) {
+        super(url, token);
+    }
+
+    /**
+     * Initialise l'instance de l'API avec le token d'accès.
+     * @param token Le token d'accès.
+     */
+    public static initInstance(token: string): void {
+        if (!PoolsApi.instance) {
+            PoolsApi.instance = new PoolsApi(CONFIG.API_POOLS_BASE_URL, token);
+        }
+    }
+
+    /**
+     * Retourne l'instance de l'API.
+     * @throws Une erreur si l'instance n'a pas été initialisée.
+     */
+    public static getInstance(): PoolsApi {
+        if (!PoolsApi.instance) {
+            throw new Error('Initialize instance before calling getInstance().');
+        }
+        return PoolsApi.instance;
+    }
+
+    /**
+     * Récupère toutes les poules.
+     * @returns Un tableau de toutes les poules.
+     */
+    public async getAllPools(): Promise<Pool[]> {
+        const response = await this.service.get<Pool[]>('/');
         return response.data;
-    },
+    }
 
-    getPoolById: async (id: number): Promise<Pool> => {
-        const response = await api.get<Pool>(`/pools/${id}`);
+    /**
+     * Récupère une poule par son identifiant.
+     * @param id - L'identifiant de la poule.
+     * @returns La poule correspondant à l'identifiant donné.
+     */
+    public async getPoolById(id: number): Promise<Pool> {
+        const response = await this.service.get<Pool>(`/${id}`);
         return response.data;
-    },
+    }
+}
 
-    createPool: async (data: Omit<Pool, 'id'>): Promise<Pool> => {
-        const response = await api.post<Pool>('/pools', data);
-        return response.data;
-    },
-};
+export default PoolsApi;
