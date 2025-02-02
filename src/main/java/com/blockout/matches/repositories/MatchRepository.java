2915de69e5e908e578d3a99eb64b4f9b3a9591ac
hugoeckenschwiller <hu.eckenschwiller@gmail.com>
2025-02-02 22:15:17 +0100

    temp filter for pools

--- src/main/java/com/blockout/matches/repositories/MatchRepository.java
+++ src/main/java/com/blockout/matches/repositories/MatchRepository.java
@@ -46,6 +46,7 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
          */
         @Query("SELECT DISTINCT CAST(m.matchDate AS LocalDate) FROM Match m " +
                         "WHERE m.matchDate <= :today " +
+                        "AND m.poolId IN (1, 2, 3) " +
                         "ORDER BY CAST(m.matchDate AS LocalDate) DESC")
         List<LocalDate> findDistinctDatesUntil(@Param("today") LocalDateTime today);
 
