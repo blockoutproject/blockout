a8a88a1ec2bb9c6a29f2eb3e72e64b3242063613
hugoeckenschwiller <hu.eckenschwiller@gmail.com>
2025-02-02 22:37:54 +0100

    fix

--- hooks/useMatchById.ts
+++ hooks/useMatchById.ts
@@ -5,19 +5,17 @@ import { Match } from '@/types/Match';
 import MatchesApi from '@/api/MatchesApi';
 
 export function useMatchById(matchId: number) {
-    // Récupérer le match depuis le cache de React Query
     const matchQuery = useQuery<Match>({
-        queryKey: ['match', matchId], // Utilisation de la clé unique pour chaque match
+        queryKey: ['match', matchId],
         queryFn: async () => {
             return MatchesApi.getInstance().getMatchById(matchId);
         },
         staleTime: 1000 * 60 * 5,
-        enabled: false, // Empêche React Query de refetch si le match n'est pas dans le cache
+        enabled: true,
     });
 
     const match = matchQuery.data;
 
-    // Récupérer les équipes associées si le match existe
     const teamAQuery = useTeamById(match?.team_id_a);
     const teamBQuery = useTeamById(match?.team_id_b);
 
