9e4b6932c0f8165410352e47d4d64a87bf0b0820
hugoeckenschwiller <hu.eckenschwiller@gmail.com>
2025-04-21 16:51:01 +0200

    refactor: remove league code exclusion for pool links in regional scraper

--- scrapers/regional_scraper.py
+++ scrapers/regional_scraper.py
@@ -151,7 +151,7 @@ class RegionalScraper(Scraper):
             # Parcours des poules
             for a_tag in pool_links:
                 # Exclusion pour tester les desactivations
-                if league_code in ['LICO']:
+                if league_code in []:
                     continue
                 try:
                     href = a_tag['href']
