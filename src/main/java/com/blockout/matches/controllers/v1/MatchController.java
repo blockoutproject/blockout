7958513677d77138bb623e64baec92d10601b248
hugoeckenschwiller <hu.eckenschwiller@gmail.com>
2025-04-21 15:27:57 +0200

    refactor: update MatchController to define endpoint mapping and remove context path from application.yaml

--- src/main/java/com/blockout/matches/controllers/v1/MatchController.java
+++ src/main/java/com/blockout/matches/controllers/v1/MatchController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1/matches")
 public class MatchController {
 
     private final MatchService matchService;
