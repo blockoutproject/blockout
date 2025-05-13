d4edc4ca76f5a2b3aca09bf93814d995993c3d5c
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-05-13 18:57:30 +0200

    refactor: update pom.xml to include Lombok version and configure maven-compiler-plugin for annotation processing

--- src/main/java/com/blockout/matches/models/dto/BulkMatchesDeactivateRequest.java
+++ src/main/java/com/blockout/matches/models/dto/BulkMatchesDeactivateRequest.java
@@ -3,14 +3,11 @@ package com.blockout.matches.models.dto;
 import lombok.Data;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 @Data
 public class BulkMatchesDeactivateRequest {
     /**
      * Liste des matchCodes qui sont encore présents dans la poule 
      * selon le scraping.
      */
-    @JsonProperty("missing_match_codes")
     private List<String> missingMatchCodes;
 }
