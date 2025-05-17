16276306541881544bc46a0449fda117b6f5975f
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-05-17 16:51:03 +0200

    refactor: add 'LICO' to excluded league codes in regional scraper

--- scrapers/regional_scraper.py
+++ scrapers/regional_scraper.py
@@ -66,7 +66,7 @@ class RegionalScraper(Scraper):
                         league_page_url = a_tag['href']
 
                         # On lance la tâche de scraping des poules pour cette ligue
-                        if league_code not in ['LIMY', 'LIGY', 'LIGU', 'LIMART', 'LIRE']:
+                        if league_code not in ['LIMY', 'LIGY', 'LIGU', 'LIMART', 'LIRE', 'LICO']:
                             task = self.scrape_pools_from_league(league_code, league_name, league_page_url)
                             tasks.append(task)
 
