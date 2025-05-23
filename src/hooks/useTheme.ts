a3002110ae8ac0cdcf7125811109743e63c6531a
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-05-23 23:42:15 +0200

    refactor: remove unused useTheme hook to streamline codebase

--- src/hooks/useTheme.ts
+++
@@ -1,6 +0,0 @@
-import { useTheme as useNavigationTheme } from '@react-navigation/native';
-import { AppTheme } from '../constants/Theme';
-
-export const useTheme = () => {
-    return useNavigationTheme() as AppTheme;
-};
