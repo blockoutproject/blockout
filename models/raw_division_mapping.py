e09b4932ba87e5f8bda5f6645d4622f147b42163
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-06-27 19:34:47 +0200

    refactor: rename updated_at to last_update for consistency in RawDivisionMapping

--- models/raw_division_mapping.py
+++ models/raw_division_mapping.py
@@ -12,7 +12,7 @@ class RawDivisionMapping:
     format: Optional[str] = None
     gender: Optional[str] = None
     created_at: datetime = datetime.now()
-    updated_at: Optional[datetime] = None
+    last_update: Optional[datetime] = None
 
     def is_mapped(self) -> bool:
         return self.division_code is not None and self.format is not None and self.gender is not None
