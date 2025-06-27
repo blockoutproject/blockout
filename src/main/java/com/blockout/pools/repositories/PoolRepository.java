68975b337c8f85b59726dd88a02971c0fb0098bb
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-06-27 20:25:37 +0200

    refactor: remove unused method findByPoolCodeAndLeagueCodeAndSeason from PoolRepository

--- src/main/java/com/blockout/pools/repositories/PoolRepository.java
+++ src/main/java/com/blockout/pools/repositories/PoolRepository.java
@@ -3,7 +3,6 @@ package com.blockout.pools.repositories;
 import com.blockout.pools.models.Pool;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,11 +11,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PoolRepository extends JpaRepository<Pool, Long> {
-    Optional<Pool> findByPoolCodeAndLeagueCodeAndSeason(String poolCode, String leagueCode, Integer season);
 
     List<Pool> findByLeagueCodeAndActive(String leagueCode, Boolean active);
-
-    List<Pool> findByLeagueCodeAndSeason(String leagueCode, Integer season);
 
     @Query("""
             SELECT p
