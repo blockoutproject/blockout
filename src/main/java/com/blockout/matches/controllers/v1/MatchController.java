5d327bcce83b16b4c0c58a77f7c586e4b2bd2cd5
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-07-08 00:42:15 +0200

    refactor: change default value for size parameter in dayGroups method to allow flexible input

--- src/main/java/com/blockout/matches/controllers/v1/MatchController.java
+++ src/main/java/com/blockout/matches/controllers/v1/MatchController.java
@@ -84,7 +84,7 @@ public class MatchController {
     @GetMapping("/day-groups")
     public ResponseEntity<DayPageDTO> dayGroups(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "3") int size,
+            @RequestParam int size,
             @RequestParam(required = false, name = "pool_ids") List<Long> poolIds,
             @RequestParam(required = false, name = "team_ids") List<Long> teamIds,
             @RequestParam(required = false) MatchStatus status) {
