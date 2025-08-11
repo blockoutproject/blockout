e7a25b1b63cab11c80e1436a81d7aff438af89e2
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-08-11 22:42:14 +0200

    refactor: Remove unused Document annotation and clean up imports in PoolSearchDoc

--- src/main/java/com/blockout/search/models/docs/PoolSearchDoc.java
+++ src/main/java/com/blockout/search/models/docs/PoolSearchDoc.java
@@ -1,18 +1,19 @@
 package com.blockout.search.models.docs;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(indexName = "pools")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PoolSearchDoc {
     @Id
     private Long id;
