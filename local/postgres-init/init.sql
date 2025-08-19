2b31a38de0185119143c28df9d235ea31bc16f6d
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-08-19 20:32:21 +0200

    fix: update Maven and JRE versions in Dockerfile; remove obsolete PostgreSQL init script

--- local/postgres-init/init.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS blockout_pools;
-DROP USER IF EXISTS 'blockout_pools_admin'@'localhost';
-CREATE database blockout_pools;
-CREATE USER IF NOT EXISTS 'blockout_pools_admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON blockout_pools.* TO 'blockout_pools_admin'@'localhost';
-FLUSH PRIVILEGES;
