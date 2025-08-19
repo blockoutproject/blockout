b675deb202f7f7961781465de82037312ac4c263
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-08-19 20:32:27 +0200

    refactor: update Maven base image version and remove obsolete PostgreSQL initialization script

--- local/postgres-init/init.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS blockout_matches;
-DROP USER IF EXISTS 'blockout_matches_admin'@'localhost';
-CREATE database blockout_matches;
-CREATE USER IF NOT EXISTS 'blockout_matches_admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON blockout_matches.* TO 'blockout_matches_admin'@'localhost';
-FLUSH PRIVILEGES;
