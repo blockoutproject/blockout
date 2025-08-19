df0d171f38c9f58965ec4a26600c355a6c2a254e
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-08-19 20:32:23 +0200

    Update Dockerfile to use Maven 3.9.11 and Eclipse Temurin 21-alpine for improved compatibility; remove obsolete PostgreSQL initialization script

--- local/postgres-init/init.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS blockout_clubs;
-DROP USER IF EXISTS 'blockout_clubs_admin'@'localhost';
-CREATE database blockout_clubs;
-CREATE USER IF NOT EXISTS 'blockout_clubs_admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON blockout_clubs.* TO 'blockout_clubs_admin'@'localhost';
-FLUSH PRIVILEGES;
