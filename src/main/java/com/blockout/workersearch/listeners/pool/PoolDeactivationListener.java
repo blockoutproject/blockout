c3dae689882c01ed5b0c5591eac04c27928ad7cc
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-08-28 16:43:32 +0200

    feat: Implement RabbitMQ listeners for club, pool, and team deactivation and upsert events

---
+++ src/main/java/com/blockout/workersearch/listeners/pool/PoolDeactivationListener.java
@@ -0,0 +1,32 @@
+package com.blockout.workersearch.listeners.pool;
+
+import com.blockout.workersearch.config.RabbitMQConfig;
+import com.blockout.workersearch.services.index.PoolIndexService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
+@Component
+@RequiredArgsConstructor
+public class PoolDeactivationListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(PoolDeactivationListener.class);
+    private final PoolIndexService poolIndexService;
+
+    @RabbitListener(queues = RabbitMQConfig.POOL_DEACTIVATION_QUEUE_SEARCH)
+    public void onPoolDeactivated(Long poolId) {
+        logger.info("Received pool deactivation event",
+                keyValue("action", "pool_deactivated"),
+                keyValue("poolId", poolId));
+
+        poolIndexService.delete(poolId);
+
+        logger.info("Pool deleted from index",
+                keyValue("action", "pool_index_delete"),
+                keyValue("poolId", poolId));
+    }
+}
