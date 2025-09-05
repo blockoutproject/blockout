ecf43f56c8fcbaf38ce4a0fef39fa71fe7846783
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-09-05 23:02:57 +0200

    refactor: Add Hibernate Validator and Jakarta EL API dependencies in pom.xml; update UnreadCountDTO with NoArgsConstructor

--- src/main/java/com/blockout/notifications/models/dto/notifications/UnreadCountDto.java
+++ src/main/java/com/blockout/notifications/models/dto/notifications/UnreadCountDto.java
@@ -2,8 +2,10 @@ package com.blockout.notifications.models.dto.notifications;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UnreadCountDTO {
     private long unread;
