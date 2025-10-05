9f7fcac0ad436819acd016c477950448aa2ff146
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-10-05 12:06:32 +0200

    refactor: simplify parse_float function and replace parse_float_set with parse_float

--- utils/html_utils.py
+++ utils/html_utils.py
@@ -15,16 +15,17 @@ def parse_int(text: str) -> int:
         return 0
 
 def parse_float(text: str) -> float:
-    try:
-        return float(text.strip().replace(",", ".")) if text else 0.0
-    except ValueError:
+    if not text:
         return 0.0
 
-def parse_float_set(text: str) -> float:
-    try:
-        return float(text.strip().replace(",", ".")) if text else 0.0
-    except ValueError:
+    text = text.strip()
+    if text == "MAX":
         return 1000.0
+
+    try:
+        return float(text.replace(",", "."))
+    except ValueError:
+        return 0.0
 
 def parse_stat_line(cols: list[str]) -> AssociationStats:
     return AssociationStats(
@@ -40,7 +41,7 @@ def parse_stat_line(cols: list[str]) -> AssociationStats:
         losses_zero_to_three=parse_int(cols[12].text),
         won_sets=parse_int(cols[13].text),
         lost_sets=parse_int(cols[14].text),
-        coef_sets=parse_float_set(cols[15].text),
+        coef_sets=parse_float(cols[15].text),
         won_points=parse_int(cols[16].text),
         lost_points=parse_int(cols[17].text),
         coef_points=parse_float(cols[18].text),
