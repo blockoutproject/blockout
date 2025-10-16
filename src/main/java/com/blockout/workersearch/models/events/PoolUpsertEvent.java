e7768c9b796ab513cd609a53608141e88fe6a2d3
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-10-16 19:03:32 +0200

    feat: Remove rawName field from PoolUpsertEvent to streamline event data structure

--- src/main/java/com/blockout/workersearch/models/events/PoolUpsertEvent.java
+++ src/main/java/com/blockout/workersearch/models/events/PoolUpsertEvent.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PoolUpsertEvent {
     private Long id;
-    private String rawName;
     private String name;
     private String shortName;
     private Long divisionId;
