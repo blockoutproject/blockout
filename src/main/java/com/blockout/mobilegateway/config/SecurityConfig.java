3d7d8b5d7997a2b6822487e8caaecfe41b08a452
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-10-18 12:25:10 +0200

    fix: Update security configuration to permit access to the correct PDF endpoint

--- src/main/java/com/blockout/mobilegateway/config/SecurityConfig.java
+++ src/main/java/com/blockout/mobilegateway/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
          */
         return http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/v1/mobile/pdf/fetch").permitAll()
+                        .requestMatchers("/api/v1/mobile/pdf").permitAll()
                         .anyRequest().authenticated())
                 .cors(withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2
