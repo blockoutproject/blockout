d0a75f5f2e7ba805ee698df2e45ba6f443b198cf
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-10-27 18:48:06 +0100

    refactor: update scraper scheduling interval from 1 minute to 60 minutes

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def schedule_scraper():
     scheduler.add_job(
         main, 
         'interval', 
-        minutes=1, 
+        minutes=60, 
         next_run_time=datetime.now(timezone.utc),
         misfire_grace_time=30,
         replace_existing=True
