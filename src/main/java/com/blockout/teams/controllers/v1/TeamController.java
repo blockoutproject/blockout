225f5e47e77bdeea86bbf1bbe4761e431b434766
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-11-25 18:54:38 +0100

    feat: add logging for JSON input in updateTeam endpoint

--- src/main/java/com/blockout/teams/controllers/v1/TeamController.java
+++ src/main/java/com/blockout/teams/controllers/v1/TeamController.java
@@ -86,6 +86,7 @@ public class TeamController {
             @RequestPart("data") String json,
             @RequestPart(value = "image", required = false) MultipartFile image
     ) throws JsonProcessingException {
+        System.out.println(json);
         TeamUpdateDTO dto = objectMapper.readValue(json, TeamUpdateDTO.class);
         Team result = teamService.updateTeam(id, dto, image);
         return ResponseEntity.ok(result);
