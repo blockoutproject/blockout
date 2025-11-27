59b3b3155b0190c94d733496438b68394fb74bc2
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-11-27 22:55:56 +0100

    refactor: update authorization checks for user update and delete endpoints

--- src/main/java/com/blockout/users/controllers/v1/UserController.java
+++ src/main/java/com/blockout/users/controllers/v1/UserController.java
@@ -59,7 +59,7 @@ public class UserController {
             @ApiResponse(responseCode = "200", description = "Utilisateur mis à jour"),
             @ApiResponse(responseCode = "404", description = "Utilisateur introuvable")
     })
-    @PreAuthorize("hasAuthority('SCOPE_update:current_user') or hasAuthority('SCOPE_update:users')")
+    @PreAuthorize("hasAuthority('SCOPE_update:current_user', 'SCOPE_update:users')")
     @PutMapping(path = "/{auth0Id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<CustomUser> updateUser(
             @PathVariable String auth0Id,
@@ -90,7 +90,7 @@ public class UserController {
             @ApiResponse(responseCode = "404", description = "Utilisateur non trouvé"),
             @ApiResponse(responseCode = "500", description = "Erreur serveur lors de la suppression")
     })
-    @PreAuthorize("hasAuthority('SCOPE_delete:current_user') or hasAuthority('SCOPE_delete:users')")
+    @PreAuthorize("hasAuthority('SCOPE_delete:current_user')")
     @DeleteMapping("/me")
     public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal Jwt jwt) throws Auth0Exception {
         String auth0Id = jwt.getSubject();
