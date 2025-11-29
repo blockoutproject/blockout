f2abf4383659525bc77cf2b49e5d53cdaee2e6af
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-11-29 13:37:21 +0100

    feat: Update AppStatusDTO and AppStatusUpdateDTO to use Instant for lastUpdate and add missing properties

--- src/main/java/com/blockout/mobilegateway/models/dto/config/AppStatusDTO.java
+++ src/main/java/com/blockout/mobilegateway/models/dto/config/AppStatusDTO.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -19,6 +19,21 @@ public class AppStatusDTO {
     @JsonProperty("image_url")
     private String imageUrl;
 
+    @JsonProperty("min_version_ios")
+    private String minVersionIos;
+
+    @JsonProperty("min_version_android")
+    private String minVersionAndroid;
+
+    @JsonProperty("store_url_ios")
+    private String storeUrlIos;
+
+    @JsonProperty("store_url_android")
+    private String storeUrlAndroid;
+
+    @JsonProperty("force_update_message")
+    private String forceUpdateMessage;
+
     @JsonProperty("last_update")
-    private LocalDateTime lastUpdate;
+    private Instant lastUpdate;
 }
--- src/main/java/com/blockout/mobilegateway/models/dto/config/AppStatusUpdateDTO.java
+++ src/main/java/com/blockout/mobilegateway/models/dto/config/AppStatusUpdateDTO.java
@@ -1,7 +1,6 @@
 package com.blockout.mobilegateway.models.dto.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +13,19 @@ public class AppStatusUpdateDTO {
 
     @JsonProperty("image_url")
     private String imageUrl;
+
+    @JsonProperty("min_version_ios")
+    private String minVersionIos;
+
+    @JsonProperty("min_version_android")
+    private String minVersionAndroid;
+
+    @JsonProperty("store_url_ios")
+    private String storeUrlIos;
+
+    @JsonProperty("store_url_android")
+    private String storeUrlAndroid;
+
+    @JsonProperty("force_update_message")
+    private String forceUpdateMessage;
 }
