d7a3181205f8f2d18087aa4b26c75ae2b52954d6
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-12-02 09:15:41 +0100

    refactor: Remove logging statements from PoolClientService, TeamClientService, and UsersClientService

--- src/main/java/com/blockout/notifications/services/clients/TeamClientService.java
+++ src/main/java/com/blockout/notifications/services/clients/TeamClientService.java
@@ -4,26 +4,18 @@ import com.blockout.notifications.config.ApiClientProperties;
 import com.blockout.notifications.models.dto.team.TeamDTO;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import static net.logstash.logback.argument.StructuredArguments.keyValue;
 
 @Service
 @RequiredArgsConstructor
 public class TeamClientService {
-
-    private static final Logger logger = LoggerFactory.getLogger(TeamClientService.class);
 
     private final ApiClientProperties apiClientProperties;
     private final ApiClientService apiClientService;
 
     public TeamDTO getTeamById(Long id) {
         String url = apiClientProperties.getTeam().getUrl() + "/" + id;
-
-        logger.info("Calling getTeamById", keyValue("id", id), keyValue("url", url));
 
         ResponseEntity<TeamDTO> response = apiClientService.getService(url, TeamDTO.class);
         return response.getBody();
