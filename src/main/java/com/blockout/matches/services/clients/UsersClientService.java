f0a7bdf7d1fc8d9bbd5789e4905ee15da85e73ea
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-12-02 09:15:44 +0100

    feat: Remove logging from getCurrentUser method in UsersClientService for cleaner code

--- src/main/java/com/blockout/matches/services/clients/UsersClientService.java
+++ src/main/java/com/blockout/matches/services/clients/UsersClientService.java
@@ -1,29 +1,21 @@
 package com.blockout.matches.services.clients;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.blockout.matches.config.ApiClientProperties;
 import com.blockout.matches.models.dto.users.CustomUserDTO;
 
-import static net.logstash.logback.argument.StructuredArguments.keyValue;
-
 @Service
 @RequiredArgsConstructor
 public class UsersClientService {
-
-    private static final Logger logger = LoggerFactory.getLogger(UsersClientService.class);
 
     private final ApiClientProperties apiClientProperties;
     private final ApiClientService apiClientService;
 
     public CustomUserDTO getCurrentUser() {
         String url = apiClientProperties.getUser().getUrl() + "/me";
-
-        logger.info("Calling getCurrentUser", keyValue("url", url));
 
         ResponseEntity<CustomUserDTO> response = apiClientService.getForward(url, CustomUserDTO.class);
         return response.getBody();
