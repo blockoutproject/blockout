0aadbe501d053e69c5056f6794f5812458738cf6
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2025-12-13 17:07:26 +0100

    fix: correct error message in CustomUserEmailAlreadyUsedException for clarity

--- src/main/java/com/blockout/users/exceptions/CustomUserEmailAlreadyUsedException.java
+++ src/main/java/com/blockout/users/exceptions/CustomUserEmailAlreadyUsedException.java
@@ -2,6 +2,6 @@ package com.blockout.users.exceptions;
 
 public class CustomUserEmailAlreadyUsedException extends RuntimeException {
     public CustomUserEmailAlreadyUsedException(String email) {
-        super("L'email avec laquelle vous essayez de vous enregistrer (" + email + ") est déjà rattachée à un compte.");
+        super("L'email avec laquelle vous essayez de vous connecter (" + email + ") est déjà rattachée à un compte.");
     }
 }
