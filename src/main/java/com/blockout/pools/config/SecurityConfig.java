f86ad75a477c407726fc71966c6c0fb84fa4a2f2
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2026-02-09 14:54:09 +0100

    feat: enable CSRF protection in SecurityConfig

--- src/main/java/com/blockout/pools/config/SecurityConfig.java
+++ src/main/java/com/blockout/pools/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll() //TODOZ
             )
+            .csrf(csrf -> csrf.disable())
             .cors(withDefaults())
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt
