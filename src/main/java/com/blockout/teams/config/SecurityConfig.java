83870a5dc27a53c4afd23123eb160afa3ed0eccb
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2026-03-03 14:56:53 +0100

    refactor: update authorization checks in TeamController and enforce authentication in SecurityConfig

--- src/main/java/com/blockout/teams/config/SecurityConfig.java
+++ src/main/java/com/blockout/teams/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll() //TODOZ
+                .anyRequest().authenticated()
             )
             .csrf(csrf -> csrf.disable())
             .cors(withDefaults())
