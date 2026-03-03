caa2c556f479397c3bbfedd4bbce6982261ac85a
Hugo Eckenschwiller <166655548+hugoecken@users.noreply.github.com>
2026-03-03 14:56:58 +0100

    fix: update SecurityConfig to require authentication for all requests and enable method security in CompetitionAssociationController

--- src/main/java/com/blockout/competitions/config/SecurityConfig.java
+++ src/main/java/com/blockout/competitions/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll() //TODOZ
+                .anyRequest().authenticated()
             )
             .cors(withDefaults())
             .csrf(csrf -> csrf.disable())
